2c8a7a33867d54672426549620480c3154df4b2f
S. M. Nahid Emon <34430845+smnahidemon@users.noreply.github.com>
2021-09-10 10:51:49 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,27 +5,14 @@ sudo useradd -m NAHID
 sudo adduser NAHID sudo
 echo 'NAHID:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo apt install python2
-sudo apt install python3
-sudo apt-get upgrade --fix-broken
+
+sudo apt-get update
+sudo apt-get install xfce4 xfce4-terminal
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
-sudo apt -y install obs-studio
-sudo apt -y install firefox
-sudo apt -y install qbittorrent
-sudo apt install nload
-sudo adduser NAHID chrome-remote-desktop
+sudo dpkg -i chrome*
+sudo apt-get install -f
 } /dev/null 2>&1
+
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
