93f0f25171daecafc8c36d81444bb0e5fa04a61a
S. M. Nahid Emon <34430845+smnahidemon@users.noreply.github.com>
2021-09-10 11:28:54 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,14 +5,26 @@ sudo useradd -m NAHID
 sudo adduser NAHID sudo
 echo 'NAHID:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+
 sudo apt update
-sudo apt -y install screen
-sudo apt -y install xfce4
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+dpkg chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo export DEBIAN_FRONTEND=noninteractive
+sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal
+sudo bash -c 'echo \\\"exec /etc/X11/Xsession /usr/bin/xfce4-session\\\" > /etc/chrome-remote-desktop-session
+sudo apt remove --assume-yes gnome-terminal
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+dpkg chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y
+sudo apt -y install obs-studio
 sudo apt -y install firefox
-sudo apt install tightvncserver
-tightvncserver :1
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
+sudo apt -y install qbittorrent
+sudo apt install nload
+        
 } /dev/null 2>&1
 
 printf "\nSetup Completed " >&2 ||
