e47323c41c6cb60cdbb055bcf4f6d330bc319b78
S. M. Nahid Emon <34430845+smnahidemon@users.noreply.github.com>
2021-09-10 11:44:07 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,28 +5,26 @@ sudo useradd -m NAHID
 sudo adduser NAHID sudo
 echo 'NAHID:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-
 sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-dpkg chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo export DEBIAN_FRONTEND=noninteractive
-sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal
-sudo bash -c 'echo \\\"exec /etc/X11/Xsession /usr/bin/xfce4-session\\\" > /etc/chrome-remote-desktop-session
+sudo DEBIAN_FRONTEND=noninteractive \
+sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal 
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo apt remove --assume-yes gnome-terminal
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-dpkg chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y
 sudo apt -y install obs-studio
 sudo apt -y install firefox
 sudo apt -y install qbittorrent
 sudo apt install nload
-        
-} /dev/null 2>&1
-
+sudo adduser NAHID chrome-remote-desktop
+} &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
